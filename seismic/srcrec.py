bb3cb90e69d37f88ffe6de63dfe3f30c2704cfe7
kch_hp <k.chrapkiewicz17@imperial.ac.uk>
2020-04-06 12:49:25 +0100

    WORKING hypersource volume has xyz and amplitude

--- seismic/srcrec.py
+++ seismic/srcrec.py
@@ -47,10 +47,14 @@ class SRs(Points3d):
  
   def spread_factors(self, srtype_ids, **kwargs):
     self.set_type(srtype_ids, **kwargs)
+    #self.sprd_fctrs = {}
+    self.hyper = {}
+
     for srid, sr in self.items():
-      self.__log.info('Calculating spread_factors for SRID ' + str(srid)) 
-      sr.spread_factors(**kwargs)
-      
+      #self.__log.info('Calculating spread_factors for SRID ' + str(srid)) 
+      #self.sprd_fctrs[srid] = HyperPointSR(sr).spread_factors(**kwargs)
+      self.hyper[srid] = HyperPointSR(sr)
+    
   # -----------------------------------------------------------------------------
 
 
@@ -103,6 +107,37 @@ class Receivers(SRs):
 # -------------------------------------------------------------------------------
 
 
+@traced
+@logged
+class HyperPointSR(object):
+  """
+  """
+  def __init__(self, pointsr, **kwargs):
+    self.rmax = 4
+    self.pointsr = pointsr
+    coords = self.pointsr.find_neighs(self.rmax, **kwargs)
+    sh = np.array(coords.shape)
+    sh[-1] += 1
+    # THIS IS THE CUBOIDAL VOLUME OF SPREAD FACTORS 
+    # IT WILL BE USED TO INJECT THE SOURCE INTO (OR INTERPOLATE RECEIVER)
+    # THE WAVEVIELD
+    self.vol = np.zeros(sh)
+    self.vol[..., :3] = coords
+    self.vol[..., 3] = 0.0 # this will store amplitude, i.e. sprd_fctrs
+    
+  # -----------------------------------------------------------------------------
+  
+  def spread_factors(self, **kwargs):
+    #self.tospread = [
+    
+    
+    return self.vol
+
+  # -----------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------
+
 
 @traced
 @logged
@@ -112,7 +147,9 @@ class PointSR(GenericPoint):
   """
   def spread_factors(self, **kwargs):
     r = 3 # SAME AS IN FULLWAVE3D
-    self.spread(r)
+    self.vol = self.spread(r)
+    #self.bounce_off
+
   
   # -----------------------------------------------------------------------------
   
@@ -213,19 +250,28 @@ class VolumeSR(Arr3d):
   """
   """
   def split(self, *args, **kwargs):
-    flags = self.flag(*args, **kwargs)
-    indices = flags == self.ext_flag
-    coords = self.coords[indices]
-    values = self[indices]
-    self.outside = np.zeros(list(values.shape) + [4])
-    self.outside[...,0:3] = coords
-    self.outside[...,3] = values
+    """
+    """
+    self.flgs = self.flag(*args, **kwargs)
+    
+    attrs = ['ins', 'at', 'out']
+    flags = [self.in_flag, self.acc_flag, self.ext_flag]
+
+    for attr, flag in zip(attrs, flags):
+      indices = self.flgs == flag
+      coords = self.coords[indices]
+      values = self[indices]
+      arr = np.zeros(list(values.shape) + [4]) # must be neater syntax
+      arr[..., 0:-1] = coords
+      arr[..., -1] = values
+      self.__log.info('Found %s %s-nodes' % (len(arr), attr))
+      setattr(self, attr, arr)
 
   # -----------------------------------------------------------------------------    
 
   def flag(self, ine, *args, **kwargs):
     """
-    ine: array of in/acc/ext nodes
+    ine: array of interior/accurately at FS/exterior nodes
     """
     self.indices = self._grid_coords_2_egrid_indices(*args, **kwargs)
     
@@ -275,10 +321,12 @@ class VolumeSR(Arr3d):
     """
     assert len(ghs) == len(iss)
     
-    vout = np.copy(self.outside)
+    vout = np.copy(self.out)
     vout[:, 0] += elef
     vout[:, 1] += efro
     vout[:, 2] += etop
+    # here we compare grid-coords, NOT grid-coords to array-indices
+    # => we don't subtract 1
 
     aghs = np.array(ghs)
 
@@ -291,6 +339,7 @@ class VolumeSR(Arr3d):
       R = np.zeros(4) # WE HAVE TO CREATE IT EVERY TIME
       R[:3] = 2 * I - G
       R[3] = -A
+      #R = Monopole(
       reflected.append(R)
       #print('O', vo)
       #print('G', G)
