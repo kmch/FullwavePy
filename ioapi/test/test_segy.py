d5b2855a13f9058b70d7e605f21265f01df9e603
kmc <k.chrapkiewicz17@imperial.ac.uk>
2020-04-22 14:50:10 +0100

    Added templates for some unit tests

---
+++ ioapi/test/test_segy.py
@@ -0,0 +1,24 @@
+"""
+Unit tests of ..segy module.
+
+(c) 2019-2020 Kajetan Chrapkiewicz.
+Copywright: Ask for permission writing to k.chrapkiewicz17@imperial.ac.uk.
+
+"""
+from unittest import TestCase
+from fullwavepy.ioapi.segy import *
+
+
+class TestFoo(TestCase):
+  """
+  """
+  def test_something_in_foo(self):
+    pass
+    # s = 'foobar'
+    # self.assertEqual(core(s, ' ', 'bar'), 'foo')
+    # self.assertEqual(core(s, 'foo', ' '), 'bar')
+    # self.assertRaises(ValueError, core, s, '', 'bar')
+    # self.assertRaises(ValueError, core, s, 'foo', '')
+
+
+# -------------------------------------------------------------------------------
